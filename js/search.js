a87899a8986585960170535aac946a872ef12701
Harsh Ankur <harshankur@outlook.com>
2018-01-26 00:23:54 +0900

    rectified Cannot Read Property of Undefined error

--- js/search.js
+++ js/search.js
@@ -1,15 +1,28 @@
-var myMovieName = document.getElementById("inputMovieName").value;
-
 var myQueryUrl = "https://api.themoviedb.org/3/search/movie?api_key=da6149720cb9476c5b6f2aaf8ae03cf4&language=en-US&query=";
 document.getElementById("searchButton").addEventListener("click", searchMovieDetails);
 
 function searchMovieDetails() {
+    //Checking if Search button is working or not
+    console.log("Beginning Search now");
+
+    var myMovieName = document.getElementById("inputMovieName").value;
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", myQueryUrl, true); // true for asynchronous 
-    xmlHttp.send(null);
+    xmlHttp.open("GET", myQueryUrl + myMovieName, true); // true for asynchronous 
+    xmlHttp.send("{}");
     xmlHttp.onreadystatechange = function () {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
             var myResponse = JSON.parse(xmlHttp.responseText);
-            alert("The story of this piece is: " + myResponse.results[0].overview);
+            
+            try {
+                var myResults = myResponse.results;
+                var myFirstResult = myResults[0];
+                var myOverview = myFirstResult.overview;
+            } catch (TypeError) {
+                console.log("Cannot Read Property of undefined error found");
+            }
+            
+            console.log("The story of this piece is: " + myOverview);
+            alert("The story of this piece is: " + myOverview);
+        }
     };
 }
