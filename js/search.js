16c154af09e21da229a011f964e666c4838e38fd
harshankur <harshankur@outlook.com>
2018-01-25 23:27:34 +0900

    Added basic overview alert for API call

--- js/search.js
+++ js/search.js
@@ -1,13 +1,15 @@
 var myMovieName = document.getElementById("inputMovieName").value;
 
+var myQueryUrl = "https://api.themoviedb.org/3/search/movie?api_key=da6149720cb9476c5b6f2aaf8ae03cf4&language=en-US&query=";
 document.getElementById("searchButton").addEventListener("click", searchMovieDetails);
 
-function searchMovieDetails(theUrl, callback) {
+function searchMovieDetails() {
     var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", myQueryUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
     xmlHttp.onreadystatechange = function () {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
-    }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-    xmlHttp.send(null);
+            var myResponse = JSON.parse(xmlHttp.responseText);
+            alert("The story of this piece is: " + myResponse.results[0].overview);
+    };
 }
