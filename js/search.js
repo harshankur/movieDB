9f5056525f97deb70782a26a8d8e92d1685e132b
Harsh Ankur <harshankur@outlook.com>
2018-01-25 23:12:30 +0900

    included javascript code beginning event listener

---
+++ js/search.js
@@ -0,0 +1,13 @@
+var myMovieName = document.getElementById("inputMovieName").value;
+
+document.getElementById("searchButton").addEventListener("click", searchMovieDetails);
+
+function searchMovieDetails(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
