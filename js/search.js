7a2602412aff9ed8f8f742563e881a2181ed8251
harshankur <harshankur@outlook.com>
2018-01-26 23:25:46 +0900

    Change Enter function in text input field with button click

--- js/search.js
+++ js/search.js
@@ -1,3 +1,15 @@
+//Change Enter function in text input field with button click
+
+document.getElementById("inputMovieName").addEventListener("keydown", function(event) {
+    console.log(event.which);
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("searchButton").click();
+    }
+});
+
+//Starting API call below
+
 var myQueryUrl = "https://api.themoviedb.org/3/search/movie?api_key=da6149720cb9476c5b6f2aaf8ae03cf4&language=en-US&query=";
 document.getElementById("searchButton").addEventListener("click", searchMovieDetails);
 
@@ -12,14 +24,10 @@ function searchMovieDetails() {
     xmlHttp.onreadystatechange = function () {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
             var myResponse = JSON.parse(xmlHttp.responseText);
-            
-            try {
-                var myResults = myResponse.results;
-                var myFirstResult = myResults[0];
-                var myOverview = myFirstResult.overview;
-            } catch (TypeError) {
-                console.log("Cannot Read Property of undefined error found");
-            }
+
+            var myResults = myResponse.results;
+            var myFirstResult = myResults[0];
+            var myOverview = myFirstResult.overview;
             
             console.log("The story of this piece is: " + myOverview);
             alert("The story of this piece is: " + myOverview);
