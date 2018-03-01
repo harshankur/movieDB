dcc3cf9a669805ec1c15b68ac1cafb6bba82de73
Harsh Ankur <harshankur@outlook.com>
2018-03-01 22:41:05 +0900

    Converted the old searchMovie js file to Obsolete

---
+++ js/searchMovieObsolete.js
@@ -0,0 +1,60 @@
+//Change Enter function in text input field with button click
+
+document.getElementById("inputMovieName").addEventListener("keydown", function(event) {
+    console.log(event.which);
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("searchButton").click();
+    }
+});
+
+//Starting API call below
+
+var myQueryUrl = "https://api.themoviedb.org/3/search/movie?api_key=da6149720cb9476c5b6f2aaf8ae03cf4&language=en-US&query=";
+document.getElementById("searchButton").addEventListener("click", searchMovieDetails);
+
+function searchMovieDetails() {
+    //Checking if Search button is working or not
+    console.log("Beginning Search now");
+
+    var myMovieName = document.getElementById("inputMovieName").value;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", myQueryUrl + myMovieName, true); // true for asynchronous 
+    xmlHttp.send("{}");
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            var myResponse = JSON.parse(xmlHttp.responseText);
+
+            var myResults = myResponse.results;
+            var myFirstResult = myResults[0];
+            var myOverview = myFirstResult.overview;
+            
+            console.log("The story of this piece is: " + myOverview);
+            alert("The story of this piece is: " + myOverview);
+        }
+    };
+}
+
+//Search Movie Details with voice
+function searchMovieDetailsWithVoice(speechMovieName) {
+    //Checking if Search button is working or not
+    console.log("Beginning Search now");
+
+    document.getElementById("inputMovieName").value = speechMovieName;
+
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", myQueryUrl + speechMovieName, true); // true for asynchronous 
+    xmlHttp.send("{}");
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            var myResponse = JSON.parse(xmlHttp.responseText);
+
+            var myResults = myResponse.results;
+            var myFirstResult = myResults[0];
+            var myOverview = myFirstResult.overview;
+
+            console.log("The story of this piece is: " + myOverview);
+            alert("The story of this piece is: " + myOverview);
+        }
+    };
+}
